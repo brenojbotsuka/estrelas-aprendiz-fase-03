260eecf7aefc15408e854a45f2688a5c626da3a0
ana cunha <ana.cunha@zup.com.br>
2026-07-07 09:54:51 -0300

    refactor(produto): adicionar tratamento de exceções e logs no cache redis

--- praticas-fase-03/src/main/java/com/github/estrelas_aprendiz/praticasfase03/carregamento_rapido/produto/ProdutoService.java
+++ praticas-fase-03/src/main/java/com/github/estrelas_aprendiz/praticasfase03/carregamento_rapido/produto/ProdutoService.java
@@ -1,65 +1,62 @@
-
-
-
 package com.github.estrelas_aprendiz.praticasfase03.carregamento_rapido.produto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.estrelas_aprendiz.praticasfase03.carregamento_rapido.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import tools.jackson.databind.ObjectMapper;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProdutoService {
     private final ProdutoRepository produtoRepository;
     private final CacheService cacheService;
     private final ObjectMapper objectMapper;
 
     public List<ProdutoResponse> listarProdutos() {
-        // Chave usada para buscar ou trocar o cache dessa lista
         String cacheKey = "products:all";
+        String cachedJson = null;
 
+        // Tenta buscar no Redis
         try {
-            // Tenta ecuperar os produtos em formato JSON armazenados no Redis
-            String cachedJson = cacheService.get(cacheKey);
+            cachedJson = cacheService.get(cacheKey);
+        } catch (Exception e) {
+            log.warn("[WARN] Redis indisponível ao tentar buscar cache. Redirecionando requisição para o banco de dados. Erro: {}", e.getMessage());
+        }
 
-            if (cachedJson != null) {
-                // Desserializa e retorna caso haja cache
+        // CACHE HIT: O Redis tinha os dados
+        if (cachedJson != null) {
+            try {
                 System.out.println("CACHE HIT");
                 return objectMapper.readValue(
                         cachedJson,
                         objectMapper.getTypeFactory().constructCollectionType(List.class, ProdutoResponse.class)
                 );
+            } catch (Exception e) {
+                log.error("[ERROR] Erro ao desserializar JSON do cache. Buscando do banco de dados.", e);
             }
-
-            System.out.println("CACHE MISS");
-
-            // Caso não houver cache, busca do banco e mapeia para DTO
-            List<ProdutoResponse> produtosDto = produtoRepository.findAll().stream()
-                    .map(p -> new ProdutoResponse(
-                            p.getId(),
-                            p.getNome(),
-                            p.getPreco(),
-                            p.getDescricao()
-                    ))
-                    .toList();
-
-            // Salva a lista de DTOs serializada no cache (expira em 10 minutos)
-            String jsonToCache = objectMapper.writeValueAsString(produtosDto);
-            cacheService.set(cacheKey, jsonToCache, 10);
-
-            return produtosDto;
-
-        } catch (Exception e) {
-            // Tratamento de erro caso a serialização ou o Redis falhem.
-            System.err.println("Erro ao manipular o cache: " + e.getMessage());
-
-            return produtoRepository.findAll().stream()
-                    .map(p -> new ProdutoResponse(p.getId(), p.getNome(), p.getPreco(), p.getDescricao()))
-                    .toList();
         }
+
+        // CACHE MISS: Busca as entidades do banco de dados
+        System.out.println("CACHE MISS");
+        List<Produto> produtos = produtoRepository.findAll();
+
+        List<ProdutoResponse> produtoResponse = produtos.stream()
+                .map(p -> new ProdutoResponse(p.getId(), p.getNome(), p.getPreco(), p.getDescricao()))
+                .toList();
+
+        // Tenta salvar a lista de response no Redis
+        try {
+            String jsonToCache = objectMapper.writeValueAsString(produtoResponse);
+            cacheService.set(cacheKey, jsonToCache);
+        } catch (Exception e) {
+            log.warn("[WARN] Redis indisponível ao tentar salvar cache. O fluxo continuará normalmente. Erro: {}", e.getMessage());
+        }
+
+        return produtoResponse;
     }
 }
 
