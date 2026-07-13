baf738e582f7691c4bf681a02e1818dd9c805043
ana cunha <ana.cunha@zup.com.br>
2026-07-13 11:08:37 -0300

    refactor(autenticacao): mover enum Role para pacote user

--- praticas-fase-03/src/main/java/com/github/estrelas_aprendiz/praticasfase03/Autenticacao/Role.java
+++
@@ -1,6 +0,0 @@
-package com.github.estrelas_aprendiz.praticasfase03.Autenticacao;
-
-public enum Role {
-    ROLE_READER,
-    ROLE_AUTHOR
-}
---
+++ praticas-fase-03/src/main/java/com/github/estrelas_aprendiz/praticasfase03/autenticacao/user/Role.java
@@ -0,0 +1,6 @@
+package com.github.estrelas_aprendiz.praticasfase03.autenticacao.user;
+
+public enum Role {
+    ROLE_READER,
+    ROLE_AUTHOR
+}
