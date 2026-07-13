df6771d37f8b65809c7a5cd7854c73c27bbb5191
Anna Jullia <anna.areas@zup.com.br>
2026-07-13 08:24:51 -0300

    "feat: cria repositorio de usuario com consulta findByEmail"

---
+++ praticas-fase-03/src/main/java/com/github/estrelas_aprendiz/praticasfase03/Autenticacao/UserRepository.java
@@ -0,0 +1,9 @@
+package com.github.estrelas_aprendiz.praticasfase03.Autenticacao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
