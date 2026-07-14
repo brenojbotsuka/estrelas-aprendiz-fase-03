2316fd3b9b4be52ed47c61f73c0ff6fff3f9819c
ana cunha <ana.cunha@zup.com.br>
2026-07-14 11:48:20 -0300

    feat(autenticacao): adicionar filtro para validar token e autenticar usuário

---
+++ praticas-fase-03/src/main/java/com/github/estrelas_aprendiz/praticasfase03/autenticacao/config/SecurityFilter.java
@@ -0,0 +1,54 @@
+package com.github.estrelas_aprendiz.praticasfase03.autenticacao.config;
+
+import com.github.estrelas_aprendiz.praticasfase03.autenticacao.auth.TokenService;
+import com.github.estrelas_aprendiz.praticasfase03.autenticacao.user.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = recoverToken(request);
+
+        if (token != null) {
+            String email = tokenService.validateTokenAndGetSubject(token);
+
+            if (email != null) {
+                UserDetails user = userRepository.findByEmail(email)
+                        .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado para o token fornecido."));
+
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return null;
+        }
+        return authHeader.substring(7);
+    }
+}
