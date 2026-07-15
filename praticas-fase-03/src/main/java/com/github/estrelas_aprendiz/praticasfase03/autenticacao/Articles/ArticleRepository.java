17d586f07e4665db87b59ec9dd4fc8a347f70ea7
Anna Jullia <anna.areas@zup.com.br>
2026-07-15 09:23:57 -0300

    "feat: cria repositorio de artigos e dto de criacao"

---
+++ praticas-fase-03/src/main/java/com/github/estrelas_aprendiz/praticasfase03/autenticacao/Articles/ArticleRepository.java
@@ -0,0 +1,7 @@
+package com.github.estrelas_aprendiz.praticasfase03.autenticacao.Articles;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
